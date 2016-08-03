71dce1a9e2db2ec5888c65825d41a6d6c0e6730c
Leandro Moreno <moreno.leandro.g@gmail.com>
2016-08-03 16:27:43 -0300

    algoritmo mejorado a 41%

--- categories/kickstart.py
+++ categories/kickstart.py
@@ -1,8 +1,39 @@
 import json
 import sys
 
-
 def build_prediction(train, test):
+    """
+    Edit here. Should return one prediction for each element in test.
+    """    
+    knowlege = {}
+    for x in train:
+        category = x.get('top_level_category')
+        price = x.get('price')
+        seller_id = x.get('seller_id')
+
+        knowlege[seller_id] = knowlege.get(seller_id, {})
+        
+        old_price = knowlege[seller_id].get(category, price)
+        
+        knowlege[seller_id][category] = old_price + price / 2
+
+    resueltados = []
+    for x in test:
+        price = x.get('price')
+        seller_id = x.get('seller_id')
+        categories = knowlege.get(seller_id, {'MLM1384': 100})
+        best_price = 999999999999
+        best_category = None
+        for cat, i in categories.iteritems():
+            result = abs(price - i)
+            if result < best_price:
+                best_price = result
+                best_category = cat
+        resueltados.append(best_category)
+
+    return resueltados
+
+def build_prediction2(train, test):
     """
     Edit here. Should return one prediction for each element in test.
     """
@@ -15,17 +46,22 @@ def build_prediction(train, test):
             knowlege[category] = price
         else:
             knowlege[category] = (price + knowlege[category]) / 2
-
+    
+    resueltados = []
     for x in test:
         price = x.get('price')
-        best = 999999999999
+        best_price = 999999999999
+        best_category = None
         for category, i in knowlege.iteritems():
-            result = price % i
+            result = abs(price - i)
+            if result < best_price:
+                best_price = result
+                best_category = category
+	        resueltados.append(best_category)
+    import ipdb; ipdb.set_trace() 
 
-    import ipdb; ipdb.set_trace()    
-
-    return ['MLM1384' for x in test]
-
+    # return ['MLM1384' for x in test]
+    return resueltados
 
 def load_data(path):
     return [json.loads(line) for line in open(path)]
