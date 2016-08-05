9bfbbaf23827afe3e17f0dd60cc9b780974befb4
Leandro Moreno <lmoreno@machinalis.com>
2016-08-05 10:59:34 -0300

    Subido a 14.75% digits

--- digits/kickstart.py
+++ digits/kickstart.py
@@ -2,11 +2,41 @@ import json
 import sys
 
 
+def exclude_counter(number_list):
+    result = 0
+    for num in number_list:
+        if num > 160:
+            result = result + 1
+    return result
+
+
 def build_prediction(train, test):
     """
     Edit here. Should return one prediction for each element in test.
     """
-    return ['4' for x in test]
+    know = {}
+    for number in train:
+        image = number.get('image')
+        label = number.get('label')
+        stored_count = know.get(label)
+        if stored_count:
+            know[label] = exclude_counter(image) + stored_count / 2
+        else:
+            know[label] = exclude_counter(image)
+
+    output = []
+    for number in test:
+        count = exclude_counter(number.get('image'))
+        best_count = 999999999999
+        best_label = None
+        for label, know_count in know.iteritems():
+            result = abs(count - know_count)
+            if result < best_count:
+                best_count = result
+                best_label = label
+        output.append(best_label)
+
+    return output
 
 
 def load_data(path):
