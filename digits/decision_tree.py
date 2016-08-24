28d6d42afc139c29fdec7b0f7fbe292f4a24c36d
Leandro Moreno <lmoreno@machinalis.com>
2016-08-24 15:15:08 -0300

    Agregado TSVD. Agregado resultados de ejecuciones

--- digits/decision_tree.py
+++ digits/decision_tree.py
@@ -2,7 +2,7 @@ import json
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import cross_val_score
 from sklearn.pipeline import Pipeline
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, TruncatedSVD
 
 
 def build_prediction(train, test):
@@ -12,32 +12,33 @@ def build_prediction(train, test):
     X_test = [x['image'] for x in test]
 
     c = DecisionTreeClassifier()
-    pca = PCA()
-    p = Pipeline(steps=[
+    pca = PCA(n_components=3)
+    tsvd = TruncatedSVD(n_components=30)
+
+    # PCA
+    p1 = Pipeline(steps=[
         ('pca', pca),
+        ('clf', DecisionTreeClassifier())
+    ])
+
+    # TSVD
+    p2 = Pipeline(steps=[
+        ('tsvd', tsvd),
         ('clf', DecisionTreeClassifier())
     ])
 
     # X_train = pca.fit_transform(X_train)
 
     accuracy = cross_val_score(
-        p,                   # The pipeline
+        p2,                   # The pipeline
         X_train, y_train,    # Train data, used for cross validation
         scoring="accuracy",  # Evaluate the accuracy of the classifier
         cv=10,               # 10-fold cross validation
     ).mean()
     print "Estimated accuracy: %s" % (accuracy*100)
 
-    # TODO:
-    # Crear un pipeline con:
-    # Vectorizer (en este caso es image solo, el vectorizer no haria nada)
-    # Reductor dimension (sklearn.manifold.TSNE)
-    # clasificator
-    # Llamar p.fit(X_train, y_train)
-    # llamar p.predict(X_test)
-
-    p.fit(X_train, y_train)
-    yhat = p.predict(X_test)
+    p2.fit(X_train, y_train)
+    yhat = p2.predict(X_test)
     return yhat
 
 
@@ -57,3 +58,45 @@ if __name__ == "__main__":
             }
             out.write(json.dumps(d))
             out.write("\n")
+
+
+"""
+PCA n_components = 2
+Estimated accuracy: 81.118473539
+real    46m35.815s
+user    30m23.487s
+sys 0m5.234s
+
+TSVD
+n_components = 20
+train[:1000]
+Estimated accuracy: 100.0
+real    0m6.561s
+user    0m6.214s
+sys 0m0.339s
+
+n_components = 20
+train[:10000]
+Estimated accuracy: 99.58000979
+real    0m17.549s
+user    0m17.050s
+sys 0m0.480s
+
+n_components = 30
+Estimated accuracy: 84.6533297756
+real    2m9.632s
+user    2m8.271s
+sys 0m1.235s
+
+n_components = 40
+Estimated accuracy: 84.216700006
+real    2m38.248s
+user    2m36.739s
+sys 0m1.219s
+
+n_components = 50
+Estimated accuracy: 83.8850440987
+real    3m2.176s
+user    3m0.932s
+sys 0m1.115s
+"""
