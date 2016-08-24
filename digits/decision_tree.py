30e3dcf2f504460dc4bd4349860e913b9ac47476
Leandro Moreno <lmoreno@machinalis.com>
2016-08-24 14:31:07 -0300

    Agregando PCA para disminuir el espacio vectorial

--- digits/decision_tree.py
+++ digits/decision_tree.py
@@ -1,26 +1,43 @@
 import json
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
 
 
 def build_prediction(train, test):
     # Train
-
     X_train = [x['image'] for x in train]
     y_train = [x["label"] for x in train]
     X_test = [x['image'] for x in test]
+
     c = DecisionTreeClassifier()
+    pca = PCA()
+    p = Pipeline(steps=[
+        ('pca', pca),
+        ('clf', DecisionTreeClassifier())
+    ])
+
+    # X_train = pca.fit_transform(X_train)
 
     accuracy = cross_val_score(
-        c,                   # The classifier
+        p,                   # The pipeline
         X_train, y_train,    # Train data, used for cross validation
         scoring="accuracy",  # Evaluate the accuracy of the classifier
         cv=10,               # 10-fold cross validation
     ).mean()
     print "Estimated accuracy: %s" % (accuracy*100)
 
-    c.fit(X_train, y_train)
-    yhat = c.predict(X_test)
+    # TODO:
+    # Crear un pipeline con:
+    # Vectorizer (en este caso es image solo, el vectorizer no haria nada)
+    # Reductor dimension (sklearn.manifold.TSNE)
+    # clasificator
+    # Llamar p.fit(X_train, y_train)
+    # llamar p.predict(X_test)
+
+    p.fit(X_train, y_train)
+    yhat = p.predict(X_test)
     return yhat
 
 
